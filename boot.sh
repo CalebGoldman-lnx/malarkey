b4392bb7ecfb0d6f815be721608a98446fbb0a52
Caleb Goldman <caleb.goldmanlnx@gmail.com>
2025-09-30 15:08:21 -0600

    Temporarily fixing repo errors

--- boot.sh
+++ boot.sh
@@ -13,9 +13,9 @@ sudo pacman -Syu --noconfirm --needed git
 # Use custom repo if specified, otherwise default to basecamp/omarchy
 OMARCHY_REPO="${OMARCHY_REPO:-CalebGoldman-lnx/malarkey}"
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
+echo -e "\nCloning Omarchy from: https://github.com/CalebGoldman-lnx/malarkey.git"
 rm -rf ~/.local/share/omarchy/
-git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
+git clone "https://github.com/CalebGoldman-lnx/malarkey.git" ~/.local/share/omarchy >/dev/null
 
 # Use custom branch if instructed, otherwise default to master
 OMARCHY_REF="${OMARCHY_REF:-master}"
